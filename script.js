a9b22863bb2a294b928742ef16e74d7994400d0e
Zaeem Akhtar <zmaktr@gmail.com>
2022-03-27 23:00:58 +0500

    final for approval

--- script.js
+++ script.js
@@ -2,12 +2,41 @@
 function initMap(){
 
     // Map
+    const origin = { lat: 52.51359401929304, lng: 13.31516720471653 };
     const map = new google.maps.Map(document.getElementById('map'), {
-       center: { lat: 52.51359401929304, lng: 13.31516720471653 },
-        zoom: 13,
-        mapId: 'db46cc0368eab8f9',
+       center: origin,
+        zoom: 12    ,
+        mapId: 'a3a5790156aa9070',
         mapTypeControl: false,
         streetViewControl: false,
+        });
+        var placesService = new google.maps.places.PlacesService(map);
+
+        map.addListener('click', function(event) {
+            if (event.placeId) {
+              event.stop();
+              placesService.getDetails({placeId: event.placeId}, function(place, status) {
+                  var content = '<div>'+
+                    '<div >'+
+                    '</div>'+
+                    '<h2 class="firstHeading">' + place.name + '</h2>'+
+                    '<div">'+
+                    '<p>' + place.formatted_address + '</p>'+
+                    '<p>' + place.formatted_phone_number + '</p>'+
+                    '<p><a href="' + place.website + '">' + place.website + '</a></p>';
+                    for (var i = 0; i < 6; i++) {
+                      content += '<img src="' + place.photos[i].getUrl({maxHeight: 50}) + '">';
+                    }
+                    content += '</div></div>';
+
+                  var infoWindow = new google.maps.InfoWindow({
+                      maxWidth: 300,
+                      content: content,
+                      position: event.latLng
+                  });
+                  infoWindow.open(map);
+              });
+            }
         });
 
     // Markers  
@@ -17,156 +46,24 @@ function initMap(){
             52.51359401929304,                  // lat [0]
             13.31516720471653,                  // lng [1]
             'map-uploads/Stay Urban.png',       // URI [2]
-            78,                                 // x - anchor [3]
-            110,                                // y - anchor [4]
-            155,                                // x - scaledSize [5]
-            110,                                // y- scaledSize [6]
+            82.5,                                // x - anchor [3]
+            75,                                 // y - anchor [4]
+            165,                             // x - scaledSize [5]
+            75,                              // y- scaledSize [6]
             1,                                  // zIndex [7]
             '<div>' +                           // InfoWindow  [8]
-            '<p> Leibnizstrasse 3-4 <br/>' +
-            '10625 Berlin, Germany </p>'+ 
+            '<h1>' + 'Stayurban' + '</h1>'+
+            '<p> Leibnizstrasse 3-4 0625 Berlin, Germany<br/>' +
+            '<p> +49(0)341393784184</p>'+
+            '<p><a href="https://stayurban.de"> stayurban.de </a></p>' +
+            '<div> <img src="map-uploads/exterior.jpg" alt="exterior" height="80"> <img src="map-uploads/apartment1.jpg" alt="apartment1" height="80"><img src="map-uploads/apartment2.jpg" alt="apartment2" height="80"></div>' +
             '<a href="https://goo.gl/maps/rdH9zDgTMdDi6qMi7" target="_blank" rel="noreferrer noopener">' +
             '<button style="color:#f5f1e8" class="btn-map">' +
             'ROUTE' + 
             '</button>' +
             '</a>' +
             "</div>",
-        ],
-        // TU Berlin
-        [
-            52.51655515035668,                  // lat [0]
-            13.323722701289036,                 // lng [1]
-            "map-uploads/TU Berlin.png",                    // URI [2]
-            0,                                  // x - anchor [3]
-            32,                                 // y - anchor [4]
-            120,                                // x - scaledSize [5]
-            32,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> MAR Gebäude, Marchstraße 23, <br/>' +
-            '10587 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // Burgeramt
-        [
-            52.489461946879395,                 // lat [0]
-            13.31224367139423,                  // lng [1]
-            'map-uploads/Bürgeramt Hohenzollerndamm.png',   // URI [2]
-            56.5,                               // x - anchor [3]
-            38,                                 // y - anchor [4]
-            147,                                // x - scaledSize [5]
-            50,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Hohenzollerndamm 177, <br/>' +
-            '10713 Berlin, Germany</p>'+ 
-            "</div>",
-        ],
-        // Siemens AG
-        [
-            52.530733680948984,                 // lat [0]
-            13.270010484837519,                 // lng [1]
-            'map-uploads/Siemens AG.png',                   // URI [2]
-            113,                                // x - anchor [3]
-            0,                                  // y - anchor [4]
-            113,                                // x - scaledSize [5]
-            38,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Nonnendammallee 104, <br/>' +
-            '13629 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // Deloitte
-        [
-            52.5046429615231,                   // lat [0]
-            13.329935059999778,                 // lng [1]
-            'map-uploads/Deloitte.png',                     // URI [2]
-            98,                                 // x - anchor [3]
-            0,                                  // y - anchor [4]
-            98,                                 // x - scaledSize [5]
-            30,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Kurfürstendamm 23, <br/>' +
-            '10719 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // Deutsche Oper
-        [
-            52.51200613537274,                  // lat [0]
-            13.308397909922746,                 // lng [1]
-            'map-uploads/Deutsche Oper.png',                // URI [2]
-            140,                                // x - anchor [3]
-            0,                                  // y - anchor [4]
-            140,                                // x - scaledSize [5]
-            30,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Bismarckstraße 35, <br/>' +
-            '10627 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // Taylor Wessing
-        [
-            52.511045864602266,                 // lat [0]
-            13.377176857592548,                 // lng [1]
-            'map-uploads/Taylor Wessing.png',               // URI [2]
-            140,                                // x - anchor [3]
-            0,                                  // y - anchor [4]
-            140,                                // x - scaledSize [5]
-            30,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Ebertstraße 15, <br/>' +
-            '10117 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // Sony Europe
-        [
-            52.51076480244041,                  // lat [0]
-            13.372345936394833,                 // lng [1]
-            'map-uploads/Sony Europe.png',                  // URI [2]
-            140,                                // x - anchor [3]
-            33,                                 // y - anchor [4]
-            140,                                // x - scaledSize [5]
-            33,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Kemperpl. 1, <br/>' +
-            '10785 Berlin, Germany </p>'+ 
-            "</div>",
-        ],
-        // SpreeBogan
-        [
-            52.525032809061344,                 // lat [0]
-            13.346730586788718,                 // lng [1]
-            'map-uploads/Spree-Bogen.png',                  // URI [2]
-            0,                                  // x - anchor [3]
-            12,                                 // y - anchor [4]
-            136,                                // x - scaledSize [5]
-            24,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p> Kemperpl. 1, <br/>' +
-            '<p> Alt-Moabit, <br/>' +
-            '10557 Berlin, Germany </p>' +
-            "</div>",
-        ],
-        // 5Km
-        [
-            52.53029422633741,                  // lat [0]
-            13.383679644036834,                 // lng [1]
-            'map-uploads/5km radius.png',                   // URI [2]
-            6,                                  // x - anchor [3]
-            15,                                 // y - anchor [4]
-            75,                                 // x - scaledSize [5]
-            30,                                 // y- scaledSize [6]
-            0,                                  // zIndex [7]
-            '<div>' +                           // InfoWindow  [8]
-            '<p>5Km radius </p>' +
-            '</div>'
-        ],
+        ]
     ];
 
     for (let i = 0; i < markers.length; i++) {
@@ -191,18 +88,6 @@ function initMap(){
             infowindow.open(map, marker);
             });
     }
-
-    // 5KM Ring
-    const locationCircle = new google.maps.Circle({
-        strokeColor: "#ab9057",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#e8ebed",
-        fillOpacity: 0.35,
-        map: map,
-        center: { lat: 52.51359401929304, lng: 13.31516720471653 },
-        radius: 5000,
-      });
 }
 
 
